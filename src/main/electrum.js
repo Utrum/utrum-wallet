e7a1b6a06e3e835bfee1920fbf46a6e9bc6af201
Maxime Saddok <saddok.ma@gmail.com>
2018-02-07 12:03:22 +0100

    wip: electrum integration with node http server

---
+++ src/main/electrum.js
@@ -0,0 +1,25 @@
+var jayson = require('jayson');
+
+var coins = require('libwallet-mnz').coins
+var Client = require("jsonrpc-node").TCP.Client;
+
+
+function call(ticker, method, params, done) {
+  let coin = coins.get(ticker)
+  let electrumServer = coin.electrum[0]
+  var client = new Client(parseInt(electrumServer.port), electrumServer.host);
+  console.log(`firing ${method***REMOVED***`)
+  
+  var result = ''
+  client.call(method, params, function(err, response){
+    // if (err) return done(err);
+    console.log('client.call + '+ response.result)
+    console.log(response) // here we see reponse
+    done(response)
+  ***REMOVED***)
+***REMOVED***
+
+
+module.exports = {
+  call: call
+***REMOVED***
