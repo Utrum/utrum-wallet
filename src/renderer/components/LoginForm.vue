df34af96563e624aed5df541b3a98db589ca3987
etheodor <etiennetheodore@orange.fr>
2018-02-07 13:00:47 +0000

    fix: css on login

--- src/renderer/components/LoginForm.vue
+++ src/renderer/components/LoginForm.vue
@@ -1,35 +1,28 @@
 <template>
-  <div class="content-login row-main-item">
-    <img id="icon-login" src="~@/assets/icon-login-monaize.svg" height="60" class="col">
-    <span class="col">DECENTRALIZED ICO PLATFORM</span>
-    <div class="input-login">
-        <div class="row-main-item">
-            <div class="col">
-                <div class="row">
-                    <div class="col">
-                        <form id="login" class="row login" method="POST" enctype="multipart/form-data">
-                            <textarea v-model="passphrase" onblur="this.placeholder = 'Please enter your passphrase'" onfocus="this.placeholder = ''" placeholder="Please enter your passphrase" cols="40" rows="3" id="passphrase" class="" type="text" name="input-login"></textarea>
-                            <button @click.prevent="validatePassPhrase()" :class="btnSendStatus"><img src="~@/assets/icon-send-login.svg"></button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <button id="create-passphrase" class="btn-round-light">Don't have a passphrase?</button>
-    <img class="col" id="login-background" src="~@/assets/img-login.svg">
-</div>
-
+	<div class="content-login row-main-item">
+		<img id="icon-login" src="~@/assets/icon-login-monaize.svg" height="60" class="col">
+		<span class="col">DECENTRALIZED ICO PLATFORM</span>
+		<div class="col input-login">
+			<div class="row">
+				<form id="login" class="row login" method="POST" enctype="multipart/form-data">
+					<textarea v-model="passphrase" onblur="this.placeholder = 'Please enter your passphrase'" onfocus="this.placeholder = ''" placeholder="Please enter your passphrase" cols="40" rows="3" id="passphrase" class="col" type="text" name="input-login"></textarea>
+					<button @click.prevent="validatePassPhrase()" :class="btnSendStatus" class="btn-send-enabled"><img src="~@/assets/icon-send-login.svg"></button>
+				</form>
+			</div>
+		</div>
+		<button id="create-passphrase" class="btn-round-light">Don't have a passphrase?</button>
+		<img class="col" id="login-background" src="~@/assets/img-login.svg">
+	</div>
 </template>
 
 <script>
 export default {
 	name: 'login-form',
-  data() {
-    return {
+	data() {
+		return {
 			passphrase: '',
-      btnSendStatus: 'btn-send-enabled',
-    }
+			btnSendStatus: 'btn-send-enabled',
+		}
 	},
 	methods: {
 		validatePassPhrase() {
@@ -44,6 +37,7 @@ export default {
 </script>
 
 <style scoped>
+
 #login-background {
 	width: 100%;
 	position: absolute;
@@ -51,14 +45,42 @@ export default {
 	z-index: -1;
 }
 
+.input-login .row {
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+}
+
+.btn-send-enabled {
+	padding: 10px;
+	margin-right: 30px;
+	background-color: white;
+	border-radius: 100%;
+	border: none !important;
+	outline: none;
+	box-shadow: 0 5px 20px rgba(0,0,0,0.1), 0 3px 6px rgba(0,0,0,0.01);
+	height: 100%;
+	margin-top: auto;
+	margin-bottom: auto;
+
+}
+
+.btn-send-enabled img {
+	vertical-align: top;
+	opacity: 1;
+	transition: opacity 0.5s;
+}
+
 #icon-login {
 	margin-top: 10px;
+	max-height: 70px;
 	text-align: center;
 }
 
 .content-login {
 	align-items: center;
-	height: 50vh;
+	text-align: center;
+	height: 60vh;
 }
 
 .input-login .row-main-item {
@@ -75,13 +97,14 @@ export default {
 	border: 1px solid rgba(24,13,57,0.1);
 	border-radius: 4px;
 	width: 600px;
-	max-height: 100px;
+	max-height: 200px;
 	background-color: rgb(250,250,250);
 	margin-bottom: 35px;
+	padding: 15px;
 }
 
 .middle {
-  vertical-align: middle;
+	vertical-align: middle;
 }
 
 .btn-send-enabled {
@@ -91,10 +114,10 @@ export default {
 	border-radius: 100%;
 	border: none !important;
 	outline: none;
-  box-shadow: 0 5px 20px rgba(0,0,0,0.1), 0 3px 6px rgba(0,0,0,0.01);
-  height: 100%;
-  margin-top: auto;
-  margin-bottom: auto;
+	box-shadow: 0 5px 20px rgba(0,0,0,0.1), 0 3px 6px rgba(0,0,0,0.01);
+	height: 100%;
+	margin-top: auto;
+	margin-bottom: auto;
 
 }
 
@@ -111,12 +134,12 @@ export default {
 	border-radius: 100%;
 	border: none !important;
 	outline: none;
-  box-shadow: 0 5px 20px rgba(0,0,0,0.1), 0 3px 6px rgba(0,0,0,0.01);
-  height: 100%;
-  margin-top: auto;
-  margin-bottom: auto;
-  cursor: not-allowed;
-  pointer-events: none;
+	box-shadow: 0 5px 20px rgba(0,0,0,0.1), 0 3px 6px rgba(0,0,0,0.01);
+	height: 100%;
+	margin-top: auto;
+	margin-bottom: auto;
+	cursor: not-allowed;
+	pointer-events: none;
 }
 
 .btn-send-disabled img {
@@ -129,8 +152,6 @@ export default {
 	width: 100%;
 	text-align: center;
 	border: none !important;
-	padding-left: 80px;
-	padding-right: 30px;
 	outline: none;
 	background-color: transparent;
 }
@@ -143,7 +164,7 @@ export default {
 }
 
 textarea {
-    resize: none;
+	resize: none;
 }
 
 .btn-round-light {
@@ -157,4 +178,22 @@ textarea {
 	padding-right: 15px;
 	padding-left: 15px;
 }
+
+.row-main-item {
+	display: flex;
+	flex-direction: column;
+	margin-top: 20px;
+	margin-bottom: 20px;
+}
+
+.col {
+	flex-grow: 1;
+}
+
+.row {
+	margin: 0px;
+	display: flex;
+	justify-content: space-between;
+}
+
 </style>
