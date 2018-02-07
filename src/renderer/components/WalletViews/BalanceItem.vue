af55ffbf6d6bc3b83efb5c68d855e89052891b8a
Maxime Saddok <maxime@monaize.com>
2018-02-07 15:36:57 +0100

    feat: buy view integration

--- src/renderer/components/WalletViews/BalanceItem.vue
+++ src/renderer/components/WalletViews/BalanceItem.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="balance-item">
-    <h3>{{wallet.coin.name***REMOVED******REMOVED*** <b>{{balance***REMOVED******REMOVED***</b> {{wallet.ticker***REMOVED******REMOVED***</h3>
-    <p>{{wallet.address***REMOVED******REMOVED***</p>
+  <div class="row-main-item">
+    <div class="row-title">
+      <h2 id="balance-item">{{balance***REMOVED******REMOVED***</h2>
+      <h2 id="coin-item">{{wallet.ticker***REMOVED******REMOVED***</h2>
+    </div>
+    <div class="row-content">
+      <p id="us-dollar" class="col-header"><img src="@/assets/icon-usdollar.svg"/>balance_usd (USD)</p>
+      <button :id="wallet.ticker" type="button" class="btn qrcode">SEE YOUR QR CODE<img src="@/assets/icon-qrcode-select.svg"></img></button>
+    </div>
+    <div class="row-footer">
+      <p class="col-header">Your deposit {{wallet.coin.name***REMOVED******REMOVED*** address :</p>
+      <div class="card">
+        <button type="button" class="btn btn-copy-link btn-smartaddress" :data-clipboard-text="wallet.address">
+          <div :id="smartaddress-wallet.ticker" class="btn-inside-qrcode">
+            {{wallet.address***REMOVED******REMOVED***
+          </div>
+        </button>
+      </div>
+    </div>
+    <hr/>
   </div>
 </template>
 
@@ -23,7 +40,6 @@ var sb = require('satoshi-bitcoin')
   ***REMOVED***,
   mounted () {  
     let url = `http://localhost:8000/`
-    console.log(url)
     let payload = {
       "ticker": this.wallet.ticker,
       "method":"blockchain.address.get_balance",
@@ -31,21 +47,9 @@ var sb = require('satoshi-bitcoin')
         this.wallet.address
       ]
     ***REMOVED***
-    // payload = JSON.stringify(payload)
     this.$http.post(url,payload).then(response => {
       this.balance = sb.toBitcoin(response.data.confirmed)
-      console.log(response)
-    ***REMOVED***).catch(error => {
-      console.log(error.response)
     ***REMOVED***)
-    // console.log(electrumHost[0].port)
-    // let ecl = new ElectrumCli(electrumHost[0].port, electrumHost[0].host, electrumHost[0].mode)
-    // try {
-    //   this.balance = await ecl.blockchainAddress_getBalance(this.wallet.address)
-    // ***REMOVED*** catch (e) {
-    //   console.log(e)
-    // ***REMOVED***
-    // await ecl.close()
   ***REMOVED***,
   computed: {
     walletData()  {
@@ -56,5 +60,138 @@ var sb = require('satoshi-bitcoin')
 </script>
 
 <style>
+.content .btn:focus {
+  outline: none;
+  box-shadow: none;
+***REMOVED***
 
+.row-header {
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 40px;
+***REMOVED***
+
+.row-title {
+  margin: 0;
+  display: flex;
+  justify-content: flex-start;
+***REMOVED***
+
+.col-header {
+  flex-grow: 1;
+  margin: 0px;
+***REMOVED***
+
+.row-main-item {
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  margin-bottom: 20px;
+***REMOVED***
+
+#balance-item {
+  margin: 0;
+  margin-right: 5px;
+  font-weight: 300;
+  color: #180d39;
+***REMOVED***
+
+#coin-item{
+  margin: 0;
+  font-weight: 200;
+  color: #180d39;
+***REMOVED***
+
+.row-content {
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+***REMOVED***
+
+.row-content .btn:hover{
+  color: #7c398a;
+***REMOVED***
+
+.row-footer {
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 25px;
+***REMOVED***
+
+.row-footer button{
+  padding: 0px;
+  margin: 0px;
+  background-color: black;
+  border: white;
+  border-radius: 4px;
+***REMOVED***
+
+.row-footer button:hover{
+  background-color: #7c398a;
+***REMOVED*** 
+
+.row-footer button:active{
+  background-color: #7c398a;
+***REMOVED***
+
+.card {
+  box-shadow: 0 5px 20px rgba(0,0,0,0.1), 0 3px 6px rgba(0,0,0,0.01);
+  border-radius: 4px;
+  border: none;
+***REMOVED***
+
+.btn-inside-qrcode {
+  color: #687078;
+  font-weight: 400;
+  margin-left: 5px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  padding-left: 20px;
+  padding-right: 20px;
+  background-color: white;
+  border-radius: 3px;
+  border-left-radius: 0px;
+  border-top-left-radius: 0px 0px;
+  border-bottom-left-radius: 0px 0px;
+  width: 380px;
+  text-align: center;
+***REMOVED***
+
+.btn-inside-qrcode:hover {
+  color: #7c398a;
+***REMOVED***
+
+.balance {
+  font-size: 16pt;
+  font-weight: 300;
+  width: 150px;
+***REMOVED***
+
+.qrcode img {
+  padding-left: 10px;
+  padding-right: 0px;
+  margin-top: auto;
+  margin-bottom: auto;
+***REMOVED***
+
+.qrcode {
+  background-color: transparent;
+  color: #7c398a;
+  font-weight: 400;
+***REMOVED***
+
+hr {
+  display: block;
+  height: 1px;
+  border: 0;
+  border-top: 2px solid #f3f4fc;
+  margin: 1em 0;
+  padding: 0;
+***REMOVED***
 </style>
