796b26c3bbff98d90396823515df89ca13f38c75
Maxime Saddok <saddok.ma@gmail.com>
2018-02-12 12:36:05 +0100

    feat: add ExplorerLink component in balance view

---
+++ src/renderer/components/Utils/ExplorerLink.vue
@@ -0,0 +1,36 @@
+<template>
+  <a class="btn btn-copy-link btn-smartaddress" target="_blank" @click.prevent="openLink" :href="href">{{value***REMOVED******REMOVED***</a class="btn btn-copy-link btn-smartaddress">
+</template>
+
+<script>
+***REMOVED***
+  name: 'explorer',
+  props: ['ticker', 'type', 'value'],
+  data () {
+    return {
+      explorers: {
+        MNZ: 'https://www.mnzexplorer.com',
+        BTC: 'https://blockchain.info',
+        KMD: 'https://kmd.explorer.supernet.org',
+      ***REMOVED***
+    ***REMOVED***
+  ***REMOVED***,
+  methods: {
+    openLink: function(event) {
+      console.log(event)
+      event.preventDefault();
+      let link = event.target.href;
+      require("electron").shell.openExternal(link);
+    ***REMOVED***
+  ***REMOVED***,
+  computed: {
+    href() {
+      return `${this.explorers[this.ticker]***REMOVED***/${this.type***REMOVED***/${this.value***REMOVED***`
+    ***REMOVED***
+  ***REMOVED***
+***REMOVED***
+</script>
+
+<style>
+
+</style>
