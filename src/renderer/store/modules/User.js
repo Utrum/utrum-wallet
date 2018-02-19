93fb8a839a709a8ffeb5ba8c7c08b2035f542b43
etheodor <etiennetheodore@orange.fr>
2018-02-19 11:04:55 +0000

    feat: add user js

---
+++ src/renderer/store/modules/User.js
@@ -0,0 +1,48 @@
+import modules from './index'
+
+
+const state = {
+  loggedIn: false,
+  passphrase: ''
+}
+
+const getters = {
+  isLogged: (state) => {
+    return state.loggedIn
+  },
+  passphrase: (state) => {
+    return state.passphrase
+  }
+}
+
+const mutations = {
+  USER_LOGIN (state) {
+    state.loggedIn = true
+  },
+  USER_LOGOUT (state) {
+    state.loggedIn = false
+  },
+  SET_PASSPHRASE (state, passphrase) {
+    state.passphrase = passphrase
+  }
+}
+
+const actions = {
+  login ({ commit }, passphrase) {
+    commit('SET_PASSPHRASE', passphrase)
+    commit('USER_LOGIN')
+  },
+  logout({commit, dispatch}) {
+    dispatch('destroyWallets', {}, {root: true})
+    commit('SET_PASSPHRASE', '')
+    commit('USER_LOGOUT')
+  }
+}
+
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+}
