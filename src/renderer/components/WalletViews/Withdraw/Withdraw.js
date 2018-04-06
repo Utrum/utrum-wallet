19100d1eaf5f7c57fc9eddc81ebb3cdeb3162bfd
Maxime Saddok <saddok.ma@gmail.com>
2018-04-06 17:58:05 +0200

    fix: withdraw supported with new libwallet

--- src/renderer/components/WalletViews/Withdraw/Withdraw.js
+++ src/renderer/components/WalletViews/Withdraw/Withdraw.js
@@ -63,14 +63,8 @@ export default {
     sendToken() {
       this.calculateFees(this.fees[0]);
     },
-    callEstimateFee(blocks) {
-      const self = this;
-      this.wallet.electrum.getEstimateFee(blocks).then(response => {
-        self.fee = response;
-      });
-    },
-    onChange(value) {
-      this.calculateFees(value);
+    async onChange() {
+      await this.prepareTx();
     },
     hideModal() {
       this.$refs.confirmWithdraw.hide();
@@ -127,50 +121,62 @@ export default {
       this.withdraw = {
         amount: null,
         address: '',
-        coin: 'MNZ',
+        coin: value,
       };
       this.select = value;
-      this.withdraw.coin = value;
+    },
+    prepareTx() {
+      return this.estimateTransaction().then(tx => {
+        if (tx.alphaTx.outputs != null && tx.alphaTx.inputs != null) {
+          this.estimatedFee = sb.toBitcoin(tx.alphaTx.fee);
+        }
+        this.preparedTx = tx.alphaTx;
+        return this.preparedTx;
+      });
+    },
+    estimateTransaction() {
+      return this.$store.dispatch('prepareTransaction', {
+        wallet: this.wallet,
+        address: this.withdraw.address,
+        amount: sb.toSatoshi(this.withdraw.amount),
+        blocks: this.blocks,
+      });
     },
     withdrawFunds() {
       this.hideModal();
       if (this.canWithdraw && this.addressIsValid) {
-        const self = this;
-        this.wallet.electrum.listUnspent(this.wallet.address).then(response => {
-          this.wallet.ticker = self.wallet.ticker;
-
-          const tx = this.wallet.prepareTx(response, self.withdraw.address, sb.toSatoshi(self.withdraw.amount), self.fee);
-
-          self.wallet.electrum.broadcast(tx)
-          .then((response) => {
-            this.$toasted.show('Transaction sent !', {
-              icon: 'done',
-              action: [
-                {
-                  icon: 'close',
-                  onClick: (e, toastObject) => {
-                    toastObject.goAway(0);
+        this.prepareTx().then(tx => {
+          this.$store.dispatch('sendTransaction', { wallet: this.wallet, ...tx }).then(txBroadcast => {
+            if (!txBroadcast.error) {
+              this.$toasted.show('Transaction sent !', {
+                icon: 'done',
+                action: [
+                  {
+                    icon: 'close',
+                    onClick: (e, toastObject) => {
+                      toastObject.goAway(0);
+                    },
                   },
-                },
-                {
-                  icon: 'content_copy',
-                  onClick: (e, toastObject) => {
-                    toastObject.goAway(0);
-                    clipboard.writeText(response);
-                    setTimeout(() => {
-                      this.$toasted.show('Copied !', {
-                        duration: 1000,
-                        icon: 'done',
-                      });
-                    }, 800);
+                  {
+                    icon: 'content_copy',
+                    onClick: (e, toastObject) => {
+                      toastObject.goAway(0);
+                      clipboard.writeText(txBroadcast);
+                      setTimeout(() => {
+                        this.$toasted.show('Copied !', {
+                          duration: 1000,
+                          icon: 'done',
+                        });
+                      }, 800);
+                    },
                   },
-                },
-              ],
-            });
-          }).catch((error) => {
-            this.$toasted.error('Transaction not sent !', {
-              text: error.response,
-            });
+                ],
+              });
+            } else {
+              this.$toasted.error('Transaction not sent !', {
+                text: txBroadcast.error,
+              });
+            }
           });
         });
       }
@@ -196,7 +202,13 @@ export default {
       return (this.withdraw.amount <= this.getBalance && this.withdraw.amount > 0 && this.addressIsValid);
     },
     addressIsValid() {
-      if (this.withdraw.address) return bitcoinjs.address.fromBase58Check(this.withdraw.address).version > 0;
+      if (this.withdraw.address) {
+        try {
+          return bitcoinjs.address.fromBase58Check(this.withdraw.address).version > 0;
+        } catch (error) {
+          return false;
+        }
+      }
     },
   },
 };
