78d0c4d2b377568c4eca7eb2706758cbf5bbf489
7kharov <guillaume@monaize.com>
2018-06-02 21:36:39 +0200

    feat(electrum): Update persistence policy on electrum. Switch server when 3 attempt to reconnect failed

--- src/main/electrumManager.js
+++ src/main/electrumManager.js
@@ -16,28 +16,49 @@
 
 const coins = require('libwallet-mnz').coins;
 const ElectrumClient = require('electrum-client');
+const config = require('../renderer/config/config');
 
 export default class ElectrumManager {
   constructor() {
     this.clients = {};
     coins.all.forEach(coin => {
-      coin.electrum.forEach(electrumServer => {
-        // let ticker = coin.ticker;
-        // if (electrumServer.test === true) { ticker = `TEST${ticker}`; }
+      if (coin.electrum && coin.electrum.length > 0) {
+        const electrumServer = coin.electrum[0];
         this.clients[coin.ticker] = new ElectrumClient(parseInt(electrumServer.port, 10), electrumServer.host);
-      });
+      } else {
+        throw new Error(`Coin ticker ${coin.ticker} has an empty electrum list.`);
+      }
     });
   }
 
-  initClient(ticker, config) {
+  initClient(ticker, electrumConfig) {
     return this.clients[ticker]
-      .initElectrum(config)
+      .initElectrum(electrumConfig, { maxTry: config.tech.electrum.persistenceMaxRetry, callback: (() => this._fireUpNewClient(ticker)) })
     ;
   }
 
   requestClient(ticker, method, params) {
+    console.log(`Call ${method} with ${JSON.stringify(params)}`); // eslint-disable-line
     return this.clients[ticker]
       .request(method, params)
+      .catch((error) => {
+        console.log("Electrum_client catch:", error); // eslint-disable-line
+        return Promise.reject(error);
+      })
     ;
+  }
+
+  _fireUpNewClient(ticker) {
+    console.log("Kick off new client for ticker: ", ticker); // eslint-disable-line
+    let lastConfig;
+    coins.all.forEach(coin => {
+      if (coin.ticker === ticker && coin.electrum && coin.electrum.length > 0) {
+        const electrumServer = coin.electrum[Math.floor(Math.random() * coin.electrum.length)];
+        lastConfig = this.clients[ticker].electrumConfig;
+        this.clients[ticker] = null;
+        this.clients[ticker] = new ElectrumClient(parseInt(electrumServer.port, 10), electrumServer.host);
+      }
+    });
+    this.initClient(ticker, lastConfig);
   }
 }
