c54050aac519d8829c0d4360e9bf751d38162c8f
Vishal <vishal.p@techmanha.com>
2018-10-15 13:52:37 +0530

    removed vue2-filters plugin

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -21,7 +21,6 @@ import VueQrcodeReader from 'vue-qrcode-reader';
 import Vue from 'vue';
 import axios from 'axios';
 import Toasted from 'vue-toasted';
-import Vue2Filters from 'vue2-filters';
 import 'vue-loaders/dist/vue-loaders.css';
 import * as VueLoaders from 'vue-loaders';
 
@@ -36,7 +35,6 @@ Vue.use(BootstrapVue);
 Vue.use(VueQriously);
 Vue.use(VueClipboard);
 Vue.use(VueQrcodeReader);
-Vue.use(Vue2Filters);
 Vue.use(Toasted, {
   iconPack: 'material',
   position: 'top-right',
