b1814dfcc6109313db387ab6f657fa2162d14628
Vishal <vishal.p@techmanha.com>
2018-11-06 21:38:04 +0530

    added refresh transaction button on send page to refresh transaction updated build transaction and wallet  module in vuex store to get latest transaction

--- src/renderer/store/modules/Tx.js
+++ src/renderer/store/modules/Tx.js
@@ -22,15 +22,31 @@ const actions = {
     return wallet.electrum
       .getTransactionHistory(wallet.address)
       .then(transactionList => {
+        let existingTxs = rootGetters.getWalletTxs(wallet.ticker)
+        console.log({existingTxs, transactionList});
+        if(rootGetters.getWalletByTicker(wallet.ticker).isLoadingTransactions){
+          console.log(`%c load trannsaction for ${wallet.ticker} already in progress.`, "color: yellow; font-size: 20px;")
+          return Promise.resolve()
+        }
+        commit('SET_TRANSACTIONS_LOADING', wallet);
+
+        console.log(`%c buildTxHistory ${wallet.ticker} total: ${transactionList.length} ${rootGetters.getWalletByTicker(wallet.ticker).isLoadingTransactions}`,"color: orange; font-size: 20px;")
         return bluebird.mapSeries(transactionList, transaction => {
           return decodeTx(wallet, transaction)
             .then((transactionDetail) => {
-              // console.log(`Tx: ${transactionDetail.tx_hash}, confirmations: ${transactionDetail.confirmations}`);
               commit('ADD_TX', { ticker: wallet.ticker, newTx: transactionDetail }, { root: true });
               dispatch('buildSwapList', { root: true });
             })
             .catch(() => { })
           ;
+        })
+        .then((data)=>{
+          commit('SET_TRANSACTIONS_LOADED', wallet);
+          console.log(console.log(`%c buildTxHistory ${wallet.ticker} ${rootGetters.getWalletByTicker(wallet.ticker).isLoadingTransactions} done: ${transactionList.length}`,"color: green; font-size: 20px;"))
+        })
+        .catch(err=>{
+          commit('SET_TRANSACTIONS_LOADED', wallet);
+          console.log(`%c buildTxHistory ${wallet.ticker} ${rootGetters.getWalletByTicker(wallet.ticker).isLoadingTransactions} Error: ${transactionList.length}`,"color: red; font-size: 20px;")
         });
       })
     ;
@@ -49,9 +65,9 @@ const decodeTx = (wallet, tx) => {
     })
     .then((transaction) => {
       const promise = new Promise((resolve) => {
-        setTimeout(() => {
+        // setTimeout(() => {
           resolve(transaction);
-        }, 100);
+        // }, 100);
       });
       return promise;
     })
